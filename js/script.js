8004496e53fbac2799e397c8748c0bea2e0087f1
manfred-commits <manfredipiraino@gmail.com>
2021-09-20 16:51:50 +0200

    aggiunto v-for per li

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
+// Descrizione:
+// Rifare l'esercizio della to do list come fatto assieme in classe:
+// - stampare in pagina un item per ogni elemento contenuto in un array
+// - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+// - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+
+
+
+
+
+
 var app = new Vue({
 
     el: "#root",
     data:{
-        
+        todos:[
+            {
+                nome:"Fare la doccia",
+                stato:false
+            },
+            {
+                nome:"Fare la siesta",
+                stato:false
+            }
+        ]
     },
     mounted:function(){ 
 
